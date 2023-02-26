7d4c971c18f95e82c30c760bf2819fe3402add7f
Mariyselita <mcadena32@gmail.com>
2023-02-26 11:37:57 -0600

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import path from 'path';
 export default defineConfig({
     base: '/Tareas/',
     plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
 });
